7e388e8a68a4ea86fa459471071222e4b6f6996b
moses <838836778@qq.com>
2017-06-01 18:02:53 +0800

    完成遍历省市县三季列表的功能。

---
+++ app/src/main/java/com/example/moses/coolweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.moses.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by moses on 2017/5/31.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttplRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request);
+    }
+}
